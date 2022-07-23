27ad542068e898360eb66cdf3f409354f9ce02c4
HaMaAhYa <gunner.gun27@gmail.com>
2022-07-23 22:03:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+window.addEventListener("scroll",function () {
+    var navbar = document.getElementById("navbar");
+    var text = document.getElementById("text");
+    var pic = document.getElementById("pic");
+    var pic2 = document.getElementById("pic2");
+    navbar.classList.toggle("lock",window.scrollY > 0);
+    if (window.scrollY > 700){
+        text.style.opacity = "1";
+        pic.style.opacity = "1";
+        pic2.style.opacity = "1";
+    } else{
+        text.style.opacity = "0";
+        pic.style.opacity = "0";
+        pic2.style.opacity = "0";
+    }
+    })
+
+function popupjunk1() {
+    var junk = document.getElementById("showjunk1");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk2() {
+    var junk = document.getElementById("showjunk2");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk3() {
+    var junk = document.getElementById("showjunk3");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk4() {
+    var junk = document.getElementById("showjunk4");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk5() {
+    var junk = document.getElementById("showjunk5");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk6() {
+    var junk = document.getElementById("showjunk6");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk7() {
+    var junk = document.getElementById("showjunk7");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk8() {
+    var junk = document.getElementById("showjunk8");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk9() {
+    var junk = document.getElementById("showjunk9");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk10() {
+    var junk = document.getElementById("showjunk10");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk11() {
+    var junk = document.getElementById("showjunk11");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk12() {
+    var junk = document.getElementById("showjunk12");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function popupjunk13() {
+    var junk = document.getElementById("showjunk13");
+    if (junk.style.opacity === "0") {
+        junk.style.opacity = "1";
+      } else {
+        junk.style.opacity = "0";
+      }
+}
+
+function show1() {
+  var x = document.getElementById("Primary");
+  if (x.style.opacity === "0") {
+      x.style.opacity = "1";
+    } else {
+      x.style.opacity = "0";
+    }
+}
+
+function show2() {
+  var x = document.getElementById("Secondary");
+  if (x.style.opacity === "0") {
+      x.style.opacity = "1";
+    } else {
+      x.style.opacity = "0";
+    }
+}
